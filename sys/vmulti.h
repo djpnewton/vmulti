ba1bce7080fb90e18796b9c4a8173e4c232d28ba
Daniel Newton <djpnewton@gmail.com>
2010-11-18 17:29:40 +1300

    * adding a digitizer endpoint

--- sys/vmulti.h
+++ sys/vmulti.h
@@ -69,12 +69,11 @@ HID_REPORT_DESCRIPTOR DefaultReportDescriptor[] = {
     0x75, 0x10,                         //       REPORT_SIZE (16)
     0x55, 0x00,                         //       UNIT_EXPONENT (0)
     0x65, 0x00,                         //       UNIT (None)
-    0x09, 0x30,                         //       USAGE (X)
     0x35, 0x00,                         //       PHYSICAL_MINIMUM (0)
     0x46, 0x00, 0x00,                   //       PHYSICAL_MAXIMUM (0)
+    0x09, 0x30,                         //       USAGE (X)
     0x81, 0x02,                         //       INPUT (Data,Var,Abs)
     0x09, 0x31,                         //       USAGE (Y)
-    0x46, 0x00, 0x00,                   //       PHYSICAL_MAXIMUM (0)
     0x81, 0x02,                         //       INPUT (Data,Var,Abs)
     0x05, 0x0d,                         //       USAGE PAGE (Digitizers)
     0x09, 0x48,                         //       USAGE (Width)
@@ -105,12 +104,11 @@ HID_REPORT_DESCRIPTOR DefaultReportDescriptor[] = {
     0x75, 0x10,                         //       REPORT_SIZE (16)
     0x55, 0x00,                         //       UNIT_EXPONENT (0)
     0x65, 0x00,                         //       UNIT (None)
-    0x09, 0x30,                         //       USAGE (X)
     0x35, 0x00,                         //       PHYSICAL_MINIMUM (0)
     0x46, 0x00, 0x00,                   //       PHYSICAL_MAXIMUM (0)
+    0x09, 0x30,                         //       USAGE (X)
     0x81, 0x02,                         //       INPUT (Data,Var,Abs)
     0x09, 0x31,                         //       USAGE (Y)
-    0x46, 0x00, 0x00,                   //       PHYSICAL_MAXIMUM (0)
     0x81, 0x02,                         //       INPUT (Data,Var,Abs)
     0x05, 0x0d,                         //       USAGE PAGE (Digitizers)
     0x09, 0x48,                         //       USAGE (Width)
@@ -187,6 +185,40 @@ HID_REPORT_DESCRIPTOR DefaultReportDescriptor[] = {
     0x81, 0x06,                         //     Input (Data, Variable, Relative)
     0xc0,                               //   END_COLLECTION              
     0xc0,                               // END_COLLECTION     
+
+// Digitizer report starts here
+0x05, 0x0d,                         // USAGE_PAGE (Digitizers)
+    0x09, 0x02,                         // USAGE (Pen digitizer)
+    0xa1, 0x01,                         // COLLECTION (Application)
+    0x85, REPORTID_DIGI,                //   REPORT_ID (Digi)
+    0x05, 0x0d,                         //   USAGE_PAGE (Digitizers)
+    0x09, 0x20,                         //   USAGE (Stylus)
+    0xa1, 0x00,                         //   COLLECTION (Physical)
+    0x09, 0x32,                         //     USAGE (In Range)
+    0x09, 0x42,                         //     USAGE (Tip Switch)
+    0x15, 0x00,                         //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                         //     LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                         //     REPORT_SIZE (1)
+    0x95, 0x02,                         //     REPORT_COUNT (2)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0x75, 0x01,                         //     REPORT_SIZE (1)
+    0x95, 0x06,                         //     REPORT_COUNT (6)
+    0x81, 0x01,                         //     INPUT (Cnst,Ary,Abs)
+    0x05, 0x01,                         //     USAGE_PAGE (Generic Desktop)
+    0x26, 0xff, 0x7f,                   //     LOGICAL_MAXIMUM (32767)       
+    0x75, 0x10,                         //     REPORT_SIZE (16) 
+    0x95, 0x01,                         //     REPORT_COUNT (1)            
+    0x55, 0x0F,                         //     UNIT_EXPONENT (-1)           
+    0x65, 0x11,                         //     UNIT (cm,SI Linear)                  
+    0x35, 0x00,                         //     PHYSICAL_MINIMUM (0)         
+    0x45, 0x00,                         //     PHYSICAL_MAXIMUM (0)
+    0x09, 0x30,                         //     USAGE (X)                    
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)         
+    0x09, 0x31,                         //     USAGE (Y)                    
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0xc0,                               //   END_COLLECTION
+    0xc0,                               // END_COLLECTION
+
 //
 // Vendor defined report starts here
 //
