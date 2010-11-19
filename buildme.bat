4335d922b2dc01f892c9bd50dc7b9ee7679f2f12
Daniel Newton <djpnewton@gmail.com>
2010-11-19 23:54:11 +1300

    * batch file to setup separate_object_root WDK parameter which makes it easier to group all build binaries

---
+++ buildme.bat
@@ -0,0 +1,24 @@
+:: setup separate object root
+
+set separate_object_root=TRUE
+set _NTTREE=%CD%
+set OBJECT_ROOT=%CD%\obj
+
+set BINPLACE_EXCLUDE_FILE=%BASEDIR%\bin\symbad.txt
+set BINPLACE_LOG=%_NTTREE%\build_logs\binplace.log
+set BINPLACE_PDB_DLL=mspdb80.dll
+
+set NO_BINPLACE=
+
+set NTDBGFILES=1
+set NTDBGFILES_PRIVATE=0
+
+if Not Exist %_NTTREE% md %_NTTREE%
+if Not Exist %_NTTREE%\build_logs md %_NTTREE%\build_logs
+if Not Exist %OBJECT_ROOT% md %OBJECT_ROOT%
+
+
+
+:: build files
+
+build -wgc
