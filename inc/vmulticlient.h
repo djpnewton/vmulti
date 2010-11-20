1ee6e8353d0a5087da073d3d59d9a4d47998f8bf
Daniel Newton <djpnewton@gmail.com>
2010-11-21 12:54:37 +1300

    * spliting client hid code into vmulticlient library  * adding visual studio project files

---
+++ inc/vmulticlient.h
@@ -0,0 +1,20 @@
+#if !defined(_VMULTI_CLIENT_H_)
+#define _VMULTI_CLIENT_H_
+
+typedef struct _vmulti_client_t* pvmulti_client;
+
+pvmulti_client vmulti_alloc(void);
+
+void vmulti_free(pvmulti_client vmulti);
+
+BOOL vmulti_connect(pvmulti_client vmulti);
+
+void vmulti_disconnect(pvmulti_client vmulti);
+
+BOOL vmulti_update_mouse(pvmulti_client vmulti, BYTE button, USHORT x, USHORT y, BYTE wheelPosition);
+
+BOOL vmulti_update_digi(pvmulti_client vmulti, BYTE status, USHORT x, USHORT y);
+
+BOOL vmulti_update_multitouch(pvmulti_client vmulti, BYTE actualCount, BYTE status1, USHORT x1, USHORT y1, BYTE contactId1, BYTE status2, USHORT x2, USHORT y2, BYTE contactId2);
+
+#endif
--- test/testvmulti.c
+++ test/testvmulti.c
@@ -1,37 +1,9 @@
-/*++
-
-Copyright (c) Microsoft Corporation.  All rights reserved.
-
-    THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
-    KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
-    IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
-    PURPOSE.
-
-Module Name:
-
-    testvhid.c
-
-Environment:
-
-    user mode only
-
---*/
-
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <setupapi.h>
-#include <hidsdi.h>
-#include <TCHAR.h>
-#include "vmulticommon.h"
 
-#if __GNUC__
-    #define __in
-    #define __in_ecount(x)
-    typedef void* PVOID;
-    typedef PVOID HDEVINFO;
-    WINHIDSDI BOOL WINAPI HidD_SetOutputReport(HANDLE, PVOID, ULONG);
-#endif
+#include "vmulticommon.h"
+#include "vmulticlient.h"
 
 //
 // Function prototypes
@@ -42,44 +14,6 @@ SendHidRequests(
     HANDLE file,
     BYTE requestType
     );
-
-HANDLE
-SearchMatchingHwID (
-    void
-    );
-
-HANDLE
-OpenDeviceInterface (
-    __in       HDEVINFO                    HardwareDeviceInfo,
-    __in       PSP_DEVICE_INTERFACE_DATA   DeviceInterfaceData
-    );
-
-BOOLEAN
-CheckIfOurDevice(
-    HANDLE file
-    );
-
-
-//
-// Copied this structure from hidport.h
-//
-
-typedef struct _HID_DEVICE_ATTRIBUTES {
-
-    ULONG           Size;
-    //
-    // sizeof (struct _HID_DEVICE_ATTRIBUTES)
-    //
-    //
-    // Vendor ids of this hid device
-    //
-    USHORT          VendorID;
-    USHORT          ProductID;
-    USHORT          VersionNumber;
-    USHORT          Reserved[11];
-
-} HID_DEVICE_ATTRIBUTES, * PHID_DEVICE_ATTRIBUTES;
-
 
 //
 // Implementation
@@ -98,7 +32,7 @@ main(
     )
 {
     BYTE   reportId;
-    HANDLE file;
+    pvmulti_client vmulti;
 
     UNREFERENCED_PARAMETER(argv);
 
@@ -133,312 +67,34 @@ main(
     // File device
     //
 
-    file = SearchMatchingHwID();
+    vmulti = vmulti_alloc();
 
-    if (file != INVALID_HANDLE_VALUE)
-    {
-        printf("...sending write request to our device\n");
-        SendHidRequests(file, reportId);
-    }
-    else
+    if (vmulti == NULL)
     {
         return 2;
     }
 
+    if (!vmulti_connect(vmulti))
+    {
+        vmulti_free(vmulti);
+        return 3;
+    }
+
+    printf("...sending write request to our device\n");
+    SendHidRequests(vmulti, reportId);
+
+    vmulti_disconnect(vmulti);
+    vmulti_free(vmulti);
+
     return 0;
-}
-
-
-HANDLE
-SearchMatchingHwID (
-    void
-    )
-{
-    HDEVINFO                  hardwareDeviceInfo;
-    SP_DEVICE_INTERFACE_DATA  deviceInterfaceData;
-    SP_DEVINFO_DATA           devInfoData;
-    GUID                      hidguid;
-    int                       i;
-    HANDLE                    file;
-
-    HidD_GetHidGuid(&hidguid);
-
-    hardwareDeviceInfo =
-            SetupDiGetClassDevs ((LPGUID)&hidguid,
-                                            NULL,
-                                            NULL, // Define no
-                                            (DIGCF_PRESENT |
-                                            DIGCF_INTERFACEDEVICE));
-
-    if (INVALID_HANDLE_VALUE == hardwareDeviceInfo){
-        printf("SetupDiGetClassDevs failed: %x\n", GetLastError());
-        return INVALID_HANDLE_VALUE;
-    }
-
-    deviceInterfaceData.cbSize = sizeof (SP_DEVICE_INTERFACE_DATA);
-
-    devInfoData.cbSize = sizeof(SP_DEVINFO_DATA);
-
-    //
-    // Enumerate devices of this interface class
-    //
-
-    printf("\n....looking for our HID device (with UP=0xFF00 "
-                "and Usage=0x01)\n");
-
-    for(i=0; SetupDiEnumDeviceInterfaces (hardwareDeviceInfo,
-                            0, // No care about specific PDOs
-                            (LPGUID)&hidguid,
-                            i, //
-                            &deviceInterfaceData);
-                            i++ ){
-
-        //
-        // Open the device interface and Check if it is our device
-        // by matching the Usage page and Usage from Hid_Caps.
-        // If this is our device then send the hid request.
-        //
-
-        HANDLE file = OpenDeviceInterface(hardwareDeviceInfo, &deviceInterfaceData);
-
-        if (file != INVALID_HANDLE_VALUE)
-        {
-            SetupDiDestroyDeviceInfoList (hardwareDeviceInfo);
-            return file;
-        }
-
-        //
-        //device was not found so loop around.
-        //
-
-    }
-
-    printf("Failure: Could not find our HID device \n");
-
-    SetupDiDestroyDeviceInfoList (hardwareDeviceInfo);
-
-    return INVALID_HANDLE_VALUE;
-}
-
-HANDLE
-OpenDeviceInterface (
-    __in       HDEVINFO                    hardwareDeviceInfo,
-    __in       PSP_DEVICE_INTERFACE_DATA   deviceInterfaceData
-    )
-{
-    PSP_DEVICE_INTERFACE_DETAIL_DATA    deviceInterfaceDetailData = NULL;
-
-    DWORD        predictedLength = 0;
-    DWORD        requiredLength = 0;
-    HANDLE       file = INVALID_HANDLE_VALUE;
-
-    SetupDiGetDeviceInterfaceDetail(
-                            hardwareDeviceInfo,
-                            deviceInterfaceData,
-                            NULL, // probing so no output buffer yet
-                            0, // probing so output buffer length of zero
-                            &requiredLength,
-                            NULL
-                            ); // not interested in the specific dev-node
-
-    predictedLength = requiredLength;
-
-    deviceInterfaceDetailData =
-         (PSP_DEVICE_INTERFACE_DETAIL_DATA) malloc (predictedLength);
-
-    if (!deviceInterfaceDetailData)
-    {
-        printf("Error: OpenDeviceInterface: malloc failed\n");
-        goto cleanup;
-    }
-
-    deviceInterfaceDetailData->cbSize =
-                    sizeof (SP_DEVICE_INTERFACE_DETAIL_DATA);
-
-    if (!SetupDiGetDeviceInterfaceDetail(
-                            hardwareDeviceInfo,
-                            deviceInterfaceData,
-                            deviceInterfaceDetailData,
-                            predictedLength,
-                            &requiredLength,
-                            NULL))
-    {
-        printf("Error: SetupDiGetInterfaceDeviceDetail failed\n");
-        free (deviceInterfaceDetailData);
-        goto cleanup;
-    }
-
-    file = CreateFile ( deviceInterfaceDetailData->DevicePath,
-                            GENERIC_READ | GENERIC_WRITE,
-                            0, // FILE_SHARE_READ | FILE_SHARE_READ |
-                            NULL, // no SECURITY_ATTRIBUTES structure
-                            OPEN_EXISTING, // No special create flags
-                            0, // No special attributes
-                            NULL); // No template file
-
-    if (INVALID_HANDLE_VALUE == file) {
-        printf("Error: CreateFile failed: %d\n", GetLastError());
-        free (deviceInterfaceDetailData);
-        goto cleanup;
-    }
-
-    if (CheckIfOurDevice(file)){
-
-        goto cleanup;
-
-    }
-
-    CloseHandle(file);
-
-    file = INVALID_HANDLE_VALUE;
-
-cleanup:
-
-    free (deviceInterfaceDetailData);
-
-    return file;
-
-}
-
-
-BOOLEAN
-CheckIfOurDevice(
-    HANDLE file)
-{
-    PHIDP_PREPARSED_DATA Ppd = NULL; // The opaque parser info describing this device
-    HIDD_ATTRIBUTES                 Attributes; // The Attributes of this hid device.
-    HIDP_CAPS                       Caps; // The Capabilities of this hid device.
-    USAGE                           MyUsagePage = 0xff00;
-    USAGE                           MyUsage = 0x0001;
-    BOOLEAN                         result = FALSE;
-
-    if (!HidD_GetPreparsedData (file, &Ppd))
-    {
-        printf("Error: HidD_GetPreparsedData failed \n");
-        goto cleanup;
-    }
-
-    if (!HidD_GetAttributes(file, &Attributes))
-    {
-        printf("Error: HidD_GetAttributes failed \n");
-        goto cleanup;
-    }
-
-    if (Attributes.VendorID == VMULTI_VID && Attributes.ProductID == VMULTI_PID)
-    {
-        if (!HidP_GetCaps (Ppd, &Caps))
-        {
-            printf("Error: HidP_GetCaps failed \n");
-            goto cleanup;
-        }
-
-        if ((Caps.UsagePage == MyUsagePage) && (Caps.Usage == MyUsage))
-        {
-            printf("Success: Found my device.. \n");
-            result = TRUE;
-        }
-    }
-
-cleanup:
-
-    if (Ppd != NULL)
-    {
-        HidD_FreePreparsedData (Ppd);
-    }
-
-    return result;
-}
-
-BOOL
-HidOutput(
-    BOOL useSetOutputReport,
-    HANDLE file,
-    PCHAR buffer,
-    ULONG bufferSize
-    )
-{
-    ULONG bytesWritten;
-    if (useSetOutputReport)
-    {
-        //
-        // Send Hid report thru HidD_SetOutputReport API
-        //
-
-        if (!HidD_SetOutputReport(file, buffer, bufferSize))
-        {
-            printf("failed HidD_SetOutputReport %d\n", GetLastError());
-            return FALSE;
-        }
-    }
-    else
-    {
-        if (!WriteFile(file, buffer, bufferSize, &bytesWritten, NULL))
-        {
-            printf("failed WriteFile %d\n", GetLastError());
-            return FALSE;
-        }
-    }
-
-    return TRUE;
 }
 
 VOID
 SendHidRequests(
-    HANDLE file,
+    pvmulti_client vmulti,
     BYTE requestType
     )
 {
-    VMultiReportHeader* pReport = NULL;
-    VMultiMultiTouchReport* pMultiReport = NULL;
-    VMultiMouseReport* pMouseReport = NULL;
-    VMultiDigiReport* pDigiReport = NULL;
-    PCHAR buffer;
-    ULONG bufferSize;
-    BYTE inputReportSize;
-
-    //
-    // Allocate 65 bytes of memory (the report descriptor says so).
-    // We only really need 1 byte for report ID + sizeof(VMultiReportHeader) +
-    // sizeof(VMultiMouseReport/VMultiMultiTouchReport) but thats ok
-    //
-
-    bufferSize = 65;
-
-    switch (requestType)
-    {
-        case REPORTID_MTOUCH:
-            inputReportSize = sizeof(VMultiMultiTouchReport);
-            break;
-        case REPORTID_MOUSE:
-            inputReportSize = sizeof(VMultiMouseReport);
-            break;
-        case REPORTID_DIGI:
-            inputReportSize = sizeof(VMultiDigiReport);
-            break;
-    }
-
-    if (bufferSize <= sizeof(VMultiReportHeader) + inputReportSize + 1)
-    {
-        printf("bummer, hid report size too small!\n");
-        return;
-    }
-
-    buffer = (PCHAR) malloc (bufferSize);
-
-    if (!buffer )
-    {
-        printf("malloc failed\n");
-        return;
-    }
-
-    //
-    // Set the report header
-    //
-
-    pReport = (VMultiReportHeader*)buffer;
-    pReport->ReportID = REPORTID_VENDOR_01;
-    pReport->ReportLength = inputReportSize;
-
     switch (requestType)
     {
         case REPORTID_MTOUCH:
@@ -446,18 +102,17 @@ SendHidRequests(
             // Send the multitouch reports
             //
             printf("Sending multitouch report\n");
-            pMultiReport = (VMultiMultiTouchReport*)(buffer + sizeof(VMultiReportHeader));
-            pMultiReport->ReportID = REPORTID_MTOUCH;
-            pMultiReport->Touch[0].Status = MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT;
-            pMultiReport->Touch[0].XValue = 10000;
-            pMultiReport->Touch[0].YValue = 1000;
-            pMultiReport->Touch[0].Width = 0;
-            pMultiReport->Touch[0].Height = 0;
-            pMultiReport->Touch[0].ContactID = 0;
-            pMultiReport->ActualCount = 1;
-            HidOutput(FALSE, file, buffer, bufferSize);
-            pMultiReport->Touch[0].Status |= MULTI_TIPSWITCH_BIT;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            while (1)
+            {
+            printf("Sending multitouch report\n");
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT, 1000, 10000, 1, 0, 0, 0, 0);
+            Sleep(100);
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT | MULTI_TIPSWITCH_BIT, 1000, 10000, 1, 0, 0, 0, 0);
+            Sleep(100);
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT, 1000, 10000, 1, 0, 0, 0, 0);
+            Sleep(100);
+            vmulti_update_multitouch(vmulti, 1, 0, 1000, 10000, 1, 0, 0, 0, 0);
+            }
             break;
 
         case REPORTID_MOUSE:
@@ -465,13 +120,7 @@ SendHidRequests(
             // Send the mouse report
             //
             printf("Sending mouse report\n");
-            pMouseReport = (VMultiMouseReport*)(buffer + sizeof(VMultiReportHeader));
-            pMouseReport->ReportID = REPORTID_MOUSE;
-            pMouseReport->Button = 0;
-            pMouseReport->XValue = 1000;
-            pMouseReport->YValue = 10000;
-            pMouseReport->WheelPosition = 0;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_mouse(vmulti, 0, 1000, 10000, 0);
             break;
 
         case REPORTID_DIGI:
@@ -479,52 +128,30 @@ SendHidRequests(
             // Send the digitizer reports
             //
             printf("Sending digitizer report\n");
-            pDigiReport = (VMultiDigiReport*)(buffer + sizeof(VMultiReportHeader));
-            pDigiReport->ReportID = REPORTID_DIGI;
-            pDigiReport->Status = DIGI_IN_RANGE_BIT;
-            pDigiReport->XValue = 1000;
-            pDigiReport->YValue = 10000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 10000);
             Sleep(100);
-            pDigiReport->YValue = 12000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 12000);
             Sleep(100);
-            pDigiReport->YValue = 14000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 14000);
             Sleep(100);
-            pDigiReport->YValue = 16000;
-            pDigiReport->Status |= DIGI_TIPSWITCH_BIT;
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 16000);
             Sleep(100);
-            pDigiReport->YValue = 18000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 18000);
             Sleep(100);
-            pDigiReport->YValue = 20000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 20000);
             Sleep(100);
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 2000, 20000);
             Sleep(100);
-            pDigiReport->XValue = 2000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 3000, 20000);
             Sleep(100);
-            pDigiReport->XValue = 3000;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 20000);
             Sleep(100);
-            pDigiReport->Status = DIGI_IN_RANGE_BIT;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 15000);
             Sleep(100);
-            pDigiReport->YValue = 15000;
-            HidOutput(FALSE, file, buffer, bufferSize);
-            Sleep(100);
-            pDigiReport->YValue = 10000;
-            HidOutput(FALSE, file, buffer, bufferSize);
-            pDigiReport->Status = 0;
-            HidOutput(FALSE, file, buffer, bufferSize);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 10000);
+            vmulti_update_digi(vmulti, 0, 3000, 10000);
             break;
     }
-
-    free(buffer);
-
-    return;
 }
 
 
