ef1f34ee97d41a1ef2538be658431eb0fa6b9e59
Ivan Vučica <ivan@vucica.net>
2010-12-11 16:48:25 +0100

    Provided facilities for simplification of install procedure (modified buildme.bat) - copy devcon.exe, - copy coinstaller's dll, - create install_driver.bat that invokes devcon

---
+++ test/testvmulti.c
@@ -0,0 +1,173 @@
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "vmulticommon.h"
+#include "vmulticlient.h"
+
+//
+// Function prototypes
+//
+
+VOID
+SendHidRequests(
+    pvmulti_client vmulti,
+    BYTE requestType
+    );
+
+//
+// Implementation
+//
+
+void
+Usage(void)
+{
+    printf("Usage: testvmulti </multitouch | /mouse | /digitizer | /joystick>\n");
+}
+
+INT __cdecl
+main(
+    int argc,
+    PCHAR argv[]
+    )
+{
+    BYTE   reportId;
+    pvmulti_client vmulti;
+
+    UNREFERENCED_PARAMETER(argv);
+
+    //
+    // Parse command line
+    //
+
+    if (argc == 1)
+    {
+        Usage();
+        return 1;
+    }
+    if (strcmp(argv[1], "/multitouch") == 0)
+    {
+        reportId = REPORTID_MTOUCH;
+    }
+    else if (strcmp(argv[1], "/mouse") == 0)
+    {
+        reportId = REPORTID_MOUSE;
+    }
+    else if (strcmp(argv[1], "/digitizer") == 0)
+    {
+        reportId = REPORTID_DIGI;
+    }
+    else if (strcmp(argv[1], "/joystick") == 0)
+    {
+        reportId = REPORTID_JOYSTICK;
+    }
+    else
+    {
+        Usage();
+        return 1;
+    }
+
+    //
+    // File device
+    //
+
+    vmulti = vmulti_alloc();
+
+    if (vmulti == NULL)
+    {
+        return 2;
+    }
+
+    if (!vmulti_connect(vmulti))
+    {
+        vmulti_free(vmulti);
+        return 3;
+    }
+
+    printf("...sending write request to our device\n");
+    SendHidRequests(vmulti, reportId);
+
+    vmulti_disconnect(vmulti);
+    vmulti_free(vmulti);
+
+    return 0;
+}
+
+VOID
+SendHidRequests(
+    pvmulti_client vmulti,
+    BYTE requestType
+    )
+{
+    switch (requestType)
+    {
+        case REPORTID_MTOUCH:
+            //
+            // Send the multitouch reports
+            //
+            printf("Sending multitouch report\n");
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT, 1000, 10000, 0, 0, 0, 0, 0);
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT | MULTI_TIPSWITCH_BIT, 1000, 10000, 0, 0, 0, 0, 0);
+            vmulti_update_multitouch(vmulti, 1, MULTI_CONFIDENCE_BIT | MULTI_IN_RANGE_BIT, 1000, 10000, 0, 0, 0, 0, 0);
+            vmulti_update_multitouch(vmulti, 1, 0, 1000, 10000, 0, 0, 0, 0, 0);
+            break;
+
+        case REPORTID_MOUSE:
+            //
+            // Send the mouse report
+            //
+            printf("Sending mouse report\n");
+            vmulti_update_mouse(vmulti, 0, 1000, 10000, 0);
+            break;
+
+        case REPORTID_DIGI:
+            //
+            // Send the digitizer reports
+            //
+            printf("Sending digitizer report\n");
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 10000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 12000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 1000, 14000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 16000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 18000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 1000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 2000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT | DIGI_TIPSWITCH_BIT, 3000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 20000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 15000);
+            Sleep(100);
+            vmulti_update_digi(vmulti, DIGI_IN_RANGE_BIT, 3000, 10000);
+            vmulti_update_digi(vmulti, 0, 3000, 10000);
+            break;
+
+        case REPORTID_JOYSTICK:
+        {
+            //
+            // Send the joystick report
+            //
+            BYTE buttons = 0, x = 0, y = 128, throttle = 0;
+            printf("Sending joystick report\n");
+            while (1)
+            {
+                vmulti_update_joystick(vmulti, buttons, x, y, throttle);
+                buttons++;
+                x++;
+                y++;
+                throttle++;
+                Sleep(10);
+            }
+            break;
+        }
+    }
+}
+
+
