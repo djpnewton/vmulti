a7a7a5005f2ab74b15b211c05184dbc8d46af669
Ivan Vu?ica <ivan@vucica.net>
2010-12-11 16:48:25 +0100

    Provided facilities for simplification of install procedure (modified buildme.bat) - copy devcon.exe, - copy coinstaller's dll, - create install_driver.bat that invokes devcon

--- buildme.bat
+++ buildme.bat
@@ -17,8 +17,11 @@ if Not Exist %_NTTREE% md %_NTTREE%
 if Not Exist %_NTTREE%\build_logs md %_NTTREE%\build_logs
 if Not Exist %OBJECT_ROOT% md %OBJECT_ROOT%
 
+:: extra copy coinstaller from redist
+copy %BASEDIR%\redist\wdf\x86\WdfCoInstaller01009.dll bin\
 
+:: create bat file for easier installation of driver
+echo %BASEDIR%\tools\devcon\i386\devcon.exe install vmulti.inf HID\vmulti > bin\install_driver.bat
 
 :: build files
-
 build -wgc
