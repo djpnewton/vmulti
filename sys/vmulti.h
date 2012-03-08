10f9b2136b9cd8b34f9695b30690f92b69901e9c
djpnewton <djpnewton@gmail.com>
2012-03-09 10:55:09 +1300

    Contribution from Vladimir Tolochko:  - third button for absolute mouse  - support for a relative mouse HID

--- sys/vmulti.h
+++ sys/vmulti.h
@@ -125,13 +125,13 @@ HID_REPORT_DESCRIPTOR DefaultReportDescriptor[] = {
     0xa1, 0x00,                         //   COLLECTION (Physical) 
     0x05, 0x09,                         //     USAGE_PAGE (Button)
     0x19, 0x01,                         //     USAGE_MINIMUM (Button 1) 
-    0x29, 0x02,                         //     USAGE_MAXIMUM (Button 2)
+    0x29, 0x05,                         //     USAGE_MAXIMUM (Button 5)
     0x15, 0x00,                         //     LOGICAL_MINIMUM (0)    
     0x25, 0x01,                         //     LOGICAL_MAXIMUM (1)   
     0x75, 0x01,                         //     REPORT_SIZE (1)      
-    0x95, 0x02,                         //     REPORT_COUNT (2)    
+    0x95, 0x05,                         //     REPORT_COUNT (5)    
     0x81, 0x02,                         //     INPUT (Data,Var,Abs)
-    0x95, 0x06,                         //     REPORT_COUNT (6)   
+    0x95, 0x03,                         //     REPORT_COUNT (3)   
     0x81, 0x03,                         //     INPUT (Cnst,Var,Abs)    
     0x05, 0x01,                         //     USAGE_PAGE (Generic Desktop)
     0x26, 0xff, 0x7f,                   //     LOGICAL_MAXIMUM (32767)    
@@ -154,6 +154,43 @@ HID_REPORT_DESCRIPTOR DefaultReportDescriptor[] = {
     0x81, 0x06,                         //     Input (Data, Variable, Relative)
     0xc0,                               //   END_COLLECTION              
     0xc0,                               // END_COLLECTION     
+
+//
+// Relative mouse report starts here
+//
+    0x05, 0x01,                         // USAGE_PAGE (Generic Desktop)
+    0x09, 0x02,                         // USAGE (Mouse)
+    0xa1, 0x01,                         // COLLECTION (Application)
+    0x85, REPORTID_RELATIVE_MOUSE,      //   REPORT_ID (Mouse)
+    0x09, 0x01,                         //   USAGE (Pointer)
+    0xa1, 0x00,                         //   COLLECTION (Physical)
+    0x05, 0x09,                         //     USAGE_PAGE (Button)
+    0x19, 0x01,                         //     USAGE_MINIMUM (Button 1)
+    0x29, 0x05,                         //     USAGE_MAXIMUM (Button 5)
+    0x15, 0x00,                         //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                         //     LOGICAL_MAXIMUM (1)
+    0x75, 0x01,                         //     REPORT_SIZE (1)
+    0x95, 0x05,                         //     REPORT_COUNT (5)
+    0x81, 0x02,                         //     INPUT (Data,Var,Abs)
+    0x95, 0x03,                         //     REPORT_COUNT (3)
+    0x81, 0x03,                         //     INPUT (Cnst,Var,Abs)
+    0x05, 0x01,                         //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                         //     USAGE (X)
+    0x09, 0x31,                         //     USAGE (Y)
+    0x15, 0x81,                         //     Logical Minimum (-127)
+    0x25, 0x7F,                         //     Logical Maximum (127)
+    0x75, 0x08,                         //     REPORT_SIZE (8)
+    0x95, 0x02,                         //     REPORT_COUNT (2)
+    0x81, 0x06,                         //     INPUT (Data,Var,Rel)
+    0x05, 0x01,                         //     Usage Page (Generic Desktop)
+    0x09, 0x38,                         //     Usage (Wheel)
+    0x15, 0x81,                         //     Logical Minimum (-127)
+    0x25, 0x7F,                         //     Logical Maximum (127)
+    0x75, 0x08,                         //     Report Size (8)
+    0x95, 0x01,                         //     Report Count (1)
+    0x81, 0x06,                         //     Input (Data, Variable, Relative)
+    0xc0,                               //   END_COLLECTION
+    0xc0,                               // END_COLLECTION
 
 //
 // Digitizer report starts here
